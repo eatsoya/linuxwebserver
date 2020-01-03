b36fc1b1cf1fa8040c7ee65feef1144fc6e392e1
pengzheng <983015118@qq.com>
2020-01-03 14:28:32 +0800

    新建webserv.c文件，初步添加头文件及部分函数

---
+++ webserv.c
@@ -0,0 +1,49 @@
+#include "csapp.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+
+void process_trans(int fd);
+int is_static(char *uri);
+void parse_static_uri(char *uri,char *filename);
+void parse_dynamic_uri(char *uri,char *filename,char *cgiargs);
+void feed_static(int fd,char *filename,int filesize);
+void get_filetype(char *filename,char *filetype);
+void feed_dynamic(int fd,char *filename,char *cgiargs,char *method,int contentlength);
+void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *description);
+
+void process_trans(int fd)
+{
+int static_flag;
+struct stat sbuf;
+char buf[MAXLINE],method[MAXLINE],uri[MAXLINE],version[MAXLINE];
+char filename[MAXLINE],cgiargs[MAXLINE];
+int contentlength=0;
+FILE *f = fdopen(fd, "r");
+if (!f) {
+perror("Unable to open input fd");
+close(fd);
+return;
+}
+setbuf(f, 0);
+if (!fgets(buf, MAXLINE, f)) {
+fclose(f);
+return;
+}
+sscanf(buf,"%s %s %s",method,uri,version);
+if(strcasecmp(method,"GET")&&strcasecmp(method,"POST")){
+error_request(fd,method,"501","NOT Implemented","Group28 does not implement this method");
+return;
+}
